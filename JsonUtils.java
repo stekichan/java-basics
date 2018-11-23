469677505de79cb846ba981ab30616cd1d56f79d
Nachiket Sahasrabuddhe <nachikets@gmail.com>
2018-11-23 19:33:07 +0530

    Code cleanup + Documentation

--- JsonUtils.java
+++ JsonUtils.java
@@ -1,32 +1,63 @@
+/*
+ * 
+ *
+ * Coding style:
+ * -Tabs: 8 spaces.
+ * -Contents of a wrapped up bracket are aligned
+ *  with open bracket on previous line.
+ * -Members of a class start with prefix containing
+ *  abbreviation of class name.
+ *
+ */
+
+
 import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
-import java.lang.reflect.Array;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 import java.nio.file.*;
 
+/**
+ *  A class that offers APIs for parsing data from a JSON stirng. To fetch a
+ *  specified JSON element from the input, the following approach is used:
+ *  1. identify the type of JSON element. This can be either:
+ *      a. another JSON object.
+ *      b. an array (comma separated JSON objects).
+ *      c. a string (quoted expression).
+ *      d. a numeric value.
+ *      e. a boolean value (either of true/false).
+ *  2. extract the contents of element. Method to extract contents of element
+ *     are dependent upon the type of the element.
+ */
 public class JsonUtils {
+
+	/**
+	 * Types of JSON elements.
+	 */
 	public enum JElemType {
 		JT_OBJ,
-			JT_ARRAY,
-			JT_STRING,
-			JT_NUM,
-			JT_BOOL,
-			JT_INVAL;
+		JT_ARRAY,
+		JT_STRING,
+		JT_NUM,
+		JT_BOOL,
+		JT_INVAL;
 	}
+
+	/**
+	 * A class holding information relevant to JSON element.
+	 */
 	static abstract class JElem {
 		JElemType      je_type;
-		// String holding the JSON element
+		/* String holding the JSON element. */
 		String         je_content;
-		// Name in the parent object
+		/* Name in the parent object. */
 		String         je_name;
-		// The parent
+		/* The parent string.*/
 		String         je_parent;
-		// Starting index in parent
+		/* Starting index in parent string. */
 		int            je_start;
-		// Length of the string.
+		/* Length of the string. */
 		int            je_len;
 		JElem(int obj_start, String name, String parent)
 		{
@@ -34,10 +65,27 @@ public class JsonUtils {
 			this.je_start  = obj_start;
 			this.je_name   = name;
 		}
+		/**
+		 * For some of the types (JT_ARRAY, JT_OBJ) it's necessary
+		 * to know the length of the element in terms of the number
+		 * of characters. This is a no-op method for remaining elements.
+		 */
 		abstract void je_len_calc();
+
+		/**
+		 * Sets je_content.
+		 */
 		abstract void je_val_extract();
 	}
 
+	/**
+	 * A helper to search a given string into the parent object of JSON element.
+	 * @param search_string: Input string.
+	 * @param parent       : Parent string to be searched in.
+	 * @param start        : Together with end defines the search range in "parent".
+	 * @param end
+	 * @return             : A "matcher" associated with the searched string.
+	 */
 	static Matcher ju_pattern_search(String search_string, String parent,
 					 int start, int end)
 	{
@@ -46,18 +94,24 @@ public class JsonUtils {
 		return  pattern.matcher(parent.substring(start, end));
 	}
 
-	static String content_process(String input)
+	/**
+	 * Eliminates quotes from start and end. For quotes inbetween eliminates
+	 * the marker "\" preceding a quote.
+	 * @param input: Input string.
+	 * @return
+	 */
+	static String ju_content_process(String input)
 	{
 		int    i;
 		int    j;
 		char[] output = new char[input.length()];
 
 		for (i = 0, j = 0; i < input.length(); ++i) {
-			// Skip the quotes at the start and end
+			/* Skip the quotes at the start and end */
 			if (input.charAt(i) == '"' &&
 			    (i == 0 || i == input.length() - 1))
 				continue;
-			// Only handles the case of \" and \\.
+			/* Only handles the case of \" and \\ */
 			if (input.charAt(i) == '\\')
 				continue;
 			output[j] = input.charAt(i);
@@ -74,10 +128,12 @@ public class JsonUtils {
 			je_len_calc();
 			je_val_extract();
 		}
+
 		void je_len_calc()
 		{
 		// Not required
 		}
+
 		void je_val_extract()
 		{
 			int     i;
@@ -111,6 +167,7 @@ public class JsonUtils {
 				this.je_content = "Input string is invalid";
 				return;
 			}
+			/* The first one terminates the value. */
 			min_idx = Math.min(comma_idx, nxt_line_idx);
 			matcher = ju_pattern_search("(true|false)", parent,
 						    this.je_start,
@@ -123,6 +180,7 @@ public class JsonUtils {
 			return;
 		}
 	}
+
 	static class num_elem extends JElem {
 		public num_elem(int obj_start, String name, String parent)
 		{
@@ -131,20 +189,25 @@ public class JsonUtils {
 			je_len_calc();
 			je_val_extract();
 		}
+
 		void je_len_calc()
 		{
 			// Not required.
 		}
+
 		void je_val_extract()
 		{
-			int i;
-			String parent = this.je_parent;
+			int     i;
+			String  parent = this.je_parent;
 
-			// Leaving out the case of exponentials and negative
-			String search_string = "-?[1-9]\\d*|0";
-			Pattern pattern = Pattern.compile(search_string);
-			Matcher matcher = pattern.matcher(parent.substring(this.je_start,
-							  parent.length() - 1));
+			/**
+			 * Leaving out the case of exponentials and floating point numbers.
+			 * Regex borrowed from: https://stackoverflow.com/questions/2367381/how-to-extract-numbers-from-a-string-and-get-an-array-of-ints
+			 **/
+			String  search_string = "-?[1-9]\\d*|0";
+			Pattern pattern       = Pattern.compile(search_string);
+			Matcher matcher       = pattern.matcher(parent.substring(this.je_start,
+							  	parent.length() - 1));
 			if (matcher.find()) {
 				this.je_content = matcher.group();
 			} else
@@ -159,20 +222,23 @@ public class JsonUtils {
 			je_len_calc();
 			je_val_extract();
 		}
+
 		void je_len_calc()
 		{
 			// Not required
 		}
+
 		void je_val_extract()
 		{
-			int i;
-			int colon_idx;
-			// Borrowed from https://stackoverflow.com/questions/2498635/java-regex-for-matching-quoted-string-with-escaped-quotes/2498670
+			int     i;
+			int     colon_idx;
+			/* Borrowed from https://stackoverflow.com/questions/2498635/java-regex-for-matching-quoted-string-with-escaped-quotes/2498670 */
 			String  search_string ="'([^\\\\']+|\\\\([btnfr\"'\\\\]|[0-3]?[0-7]{1,2}|u[0-9a-fA-F]{4}))*'|\"([^\\\\\"]+|\\\\([btnfr\"'\\\\]|[0-3]?[0-7]{1,2}|u[0-9a-fA-F]{4}))*\"";
 			Pattern pattern = Pattern.compile(search_string);
 			Matcher matcher;
+
 			i = this.je_start;
-			// Get over the name of the field.
+			/* Get over the name of the field. */
 			while (i < this.je_parent.length() &&
 			       this.je_parent.charAt(i) != ':')
 				++i;
@@ -180,7 +246,7 @@ public class JsonUtils {
 				colon_idx = i;
 				matcher = pattern.matcher(this.je_parent.substring(colon_idx, this.je_parent.length()));
 				if (matcher.find()) {
-					this.je_content = content_process(matcher.group());
+					this.je_content = ju_content_process(matcher.group());
 					return;
 				}
 			}
@@ -189,6 +255,9 @@ public class JsonUtils {
 		}
 	}
 
+	/**
+	 * A class for elements of type array.
+	 */
 	static class arr_elem extends JElem {
 		public arr_elem(int obj_start, String name, String parent)
 		{
@@ -197,6 +266,7 @@ public class JsonUtils {
 			je_len_calc();
 			je_val_extract();
 		}
+
 		void je_len_calc()
 		{
 			int i;
@@ -246,6 +316,7 @@ public class JsonUtils {
 			}
 			this.je_len = i - this.je_start + 1;
 		}
+
 		void je_val_extract()
 		{
 			int     i;
@@ -253,7 +324,7 @@ public class JsonUtils {
 			String  search_string = "\\[";
 			Pattern pattern = Pattern.compile(search_string);
 			Matcher matcher = pattern.matcher(parent.substring(this.je_start,
-									   parent.length()));
+					  				   parent.length()));
 			if (matcher.find()) {
 				this.je_content =
 					parent.substring(this.je_start + matcher.start(),
@@ -272,6 +343,7 @@ public class JsonUtils {
 			je_len_calc();
 			je_val_extract();
 		}
+
 		/* @todo: Factor out a function common to both array and a general object. */
 		void je_len_calc()
 		{
@@ -305,6 +377,7 @@ public class JsonUtils {
 			}
 			this.je_len = i - this.je_start + 1;
 		}
+
 		void je_val_extract()
 		{
 			int     i;
@@ -319,8 +392,9 @@ public class JsonUtils {
 			}
 		}
 	}
+
 	public static JElem json_elem_create(String json, String obj_name,
-					     JElemType obj_type)
+					     				 JElemType obj_type)
 	{
 		String  search_string = "\"" + obj_name + "\"" + ":";
 		Pattern pattern = Pattern.compile(search_string);
@@ -422,30 +496,68 @@ public class JsonUtils {
 		return obj_type;
 	}
 
-	public static void main(String[] args) throws Exception
-	{
-		String data;
-		JElem     obj;
+	public static String obj_fetch(String json, String obj_name) {
 		JElemType obj_type;
-		String field_name;
-		int i;
+		JElem     obj;
 
-		if (args.length != 2) {
-			System.out.println("Usage: java JsonUtils <file-name> <filed-name>");
-			return;
-		}
-		data = new String(Files.readAllBytes(Paths.get(args[0])));
-		field_name = args[1];
-		/* Identify the type of the required object. */
-		obj_type = elem_type_identify(data, field_name);
-		if (obj_type == JElemType.JT_INVAL) {
-			System.out.println("invalid object type.");
-			return;
-		}
-		/* Create an instance of required object type. */
-		obj = json_elem_create(data, field_name, obj_type);
+		obj_type = elem_type_identify(json, obj_name);
+		if (obj_type == JElemType.JT_INVAL)
+			return null;
+		obj = json_elem_create(json, obj_name, obj_type);
 		if (obj == null)
-			return;
-		System.out.println(obj.je_content);
+			return null;
+		return obj.je_content;
 	}
+	/**
+	 * Trims the starting and ending square brackets.
+         */
+	public static String array_process(String array) {
+		/* Borrowed from https://stackoverflow.com/questions/2498635/java-regex-for-matching-quoted-string-with-escaped-quotes/2498670 */
+		String  search_string = "'([^\\\\']+|\\\\([btnfr\"'\\\\]|[0-3]?[0-7]{1,2}|u[0-9a-fA-F]{4}))*'|\"([^\\\\\"]+|\\\\([btnfr\"'\\\\]|[0-3]?[0-7]{1,2}|u[0-9a-fA-F]{4}))*\"";
+		Pattern pattern       = Pattern.compile(search_string);
+		Matcher matcher       = pattern.matcher(array);
+		String  output        = "Not available";
+
+		while (matcher.find()) {
+			if (output.equals("Not available"))
+				output = matcher.group();
+			else
+				output = output + ", " + matcher.group();
+		}
+		return output;
+	}
+
+	/**
+	 * Fetches attributes from the JSON string for a sandwich and
+	 * populates the sandwich object with them.
+	 * @param json
+	 * @return
+	 */
+
+	public static void main(String[] args) throws Exception
+        {   
+                String data;
+                JElem     obj;
+                JElemType obj_type;
+                String field_name;
+                int i;
+
+                if (args.length != 2) {
+                        System.out.println("Usage: java JsonUtils <file-name> <filed-name>"); 
+                        return;
+                }   
+                data = new String(Files.readAllBytes(Paths.get(args[0])));
+                field_name = args[1];
+                /* Identify the type of the required object. */
+                obj_type = elem_type_identify(data, field_name);
+                if (obj_type == JElemType.JT_INVAL) {
+                        System.out.println("invalid object type.");
+                        return;
+                }   
+                /* Create an instance of required object type. */
+                obj = json_elem_create(data, field_name, obj_type);
+                if (obj == null)
+                        return;
+                System.out.println(obj.je_content);
+        }
 }
